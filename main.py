d2cf57d9a4fa749c86b6647554fcde8668ef1555
julianog <>
2021-04-12 22:16:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+lista1 = [11,2,3,15,8,13,21,34]  
+# Declara lista1 con 8 valores
+plt.plot(lista1)
+# Dibuja el gráfico
+plt.show()
+plt.title("Título")   # Establece el título del gráfico
+plt.xlabel("abscisa")   # Establece el título del eje x
+plt.ylabel("ordenada")   # Establece el título del eje y
